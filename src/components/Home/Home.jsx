d62d6c3750f0ba0eca5d411880cf7ce5099051bd
isaacmirex <isaacmirex@gmail.com>
2024-04-29 23:07:52 -0500

    Home cambios

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,14 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import "./styles.css"; // Importa tu archivo CSS de estilos
 
 const Home = () => {
   return (
     <div className="home-container">
-      <header>
-        <h1>Detector de Estrés con Inteligencia Artificial</h1>
-      </header>
       <section className="content">
-        <p>
+        <header style={{ opacity: 0.7 }}> {/* Aplicamos la misma opacidad al encabezado */}
+          <h1 className="title">Detector de Estrés con Inteligencia Artificial</h1>
+        </header>
+        <p className="text">
           El detector de estrés es una aplicación web que utiliza un dispositivo hardware para recolectar datos biométricos de los usuarios. Estos datos se procesan mediante un modelo de inteligencia artificial, que luego proporciona un porcentaje de estrés para cada paciente. Los resultados se presentan en un dashboard que ofrece una visión general del estrés en la universidad, clasificando los datos por facultades con el fin de mitigar el problema.
         </p>
       </section>
